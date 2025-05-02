ff62ae5738a3bb034540676ab33932ca9051787d
Ayan Khurt <ayankhurt@gmail.com>
2025-05-02 17:45:56 +0500

    new

---
+++ server.mjs
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express()
+const PORT = process.env.PORT || 5001;
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+})
