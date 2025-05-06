67c8f22395def2f9b7f47c4803e8d3321d5de048
Ayan Khurt <ayankhurt@gmail.com>
2025-05-06 18:40:14 +0500

    new

--- server.mjs
+++ server.mjs
@@ -51,6 +51,41 @@ app.delete('/delete-product/:id', (req, res) => {
   }
 });
 
+app.put('/edit-product/:id', (req, res) => {
+  const reqBody = req.body;
+  const productId = parseInt(req.params.id, 10);
+
+  console.log('Received Product ID:', productId);
+  console.log('Received Body:', req.body);
+
+  if (isNaN(productId)) {
+    return res.status(400).json({ error: 'Invalid product ID' });
+  }
+
+  if (!reqBody?.name || !reqBody?.description || !reqBody?.price) {
+    return res.status(400).json({ error: 'Required parameters missing' });
+  }
+
+  if (isNaN(reqBody.price) || reqBody.price <= 0) {
+    return res.status(400).json({ error: 'Price must be a valid positive number' });
+  }
+
+  const productIndex = products.findIndex((product) => product.id === productId);
+  console.log('Product Index:', productIndex);
+
+  if (productIndex !== -1) {
+    products[productIndex] = {
+      id: productId,
+      name: reqBody.name,
+      price: parseFloat(reqBody.price),
+      description: reqBody.description,
+    };
+    res.status(200).json({ message: 'Product updated successfully', product: products[productIndex] });
+  } else {
+    res.status(404).json({ error: `Product with ID ${productId} not found` });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
 });
